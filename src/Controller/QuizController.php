885c0b7ca1639f99517b63f28b6c664439011548
melina <boukharimelina44@gmail.com>
2026-03-29 20:45:43 +0200

    ajout controlleur page main quiz et twig main

---
+++ src/Controller/QuizController.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuizController extends AbstractController
+{
+    #[Route(path:'/quiz' , name : 'quiz')]
+    public function quiz(Request $request) {
+        $result = null ; 
+        if($request->isMethod('POST')) {
+            $respons = $request->request->get('answer');
+        
+        if($respons === "Paris")
+            {
+                $result="Correct!";
+            } else {
+                $result="Incorrect!";
+            } 
+        }
+        return $this->render('quiz.html.twig', ['result' => $result]);
+
+    }
+
+}
