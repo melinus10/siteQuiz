27820fdd25c96e301887b8b4324821b9575245c1
melina <boukharimelina44@gmail.com>
2026-04-06 00:20:47 +0200

    affichage des réponses user et score

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Questions;
 use App\Form\QuizType;
+use App\Repository\AnswersRepository;
 use App\Repository\QuestionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,32 +14,40 @@ use Symfony\Component\Routing\Attribute\Route;
 class QuizController extends AbstractController
 {
     #[Route('quiz/{id}', name: 'quiz')]
-    public function quiz(Request $request, Questions $question , QuestionsRepository $repo ): Response
+    public function quiz(Request $request, Questions $question, QuestionsRepository $questionsRepo , AnswersRepository $AnswersRepo): Response
     {
         $form = $this->createForm(QuizType::class, null, [
             'question' => $question
         ]);
         $form->handleRequest($request);
-        $answers = $request->getSession()->get('reponses', []);
+        $sessionAnswers = $request->getSession()->get('userReponses', []);
         if ($form->isSubmitted() && $form->isValid()) {
             $userAnswerArray = $form->getData();
-            $score = 0;
-                if ($userAnswerArray['reponse']->isCorrect()) {
-                    $score++;
-                }
-            $answers[$question->getId()] = $userAnswerArray['reponse']->getId();
-            $request->getSession()->set('reponses', $answers);
-            $nextQuestion = $repo->findNextQuestion($question->getId());
-            dump($request->getSession()->all()); 
+            $sessionAnswers[$question->getId()] = $userAnswerArray['reponse']->getId();
+            $request->getSession()->set('userReponses', $sessionAnswers);
+            $nextQuestion = $questionsRepo->findNextQuestion($question->getId());
             if ($nextQuestion) {
                 return $this->redirectToRoute('quiz', ['id' => $nextQuestion->getId()]);
             } else {
+                $sessionAnswers = $request->getSession()->get('userReponses', []);
+                $results = [];
+                foreach ($sessionAnswers as $qId => $rId) {
+                    $answerObj = $AnswersRepo->find($rId);
+                    $results[] = $answerObj;
+                }
+                $score = 0;
+                foreach ($results as $res) {
+                    if ($res->isCorrect()) $score++;
+                }
+                $request->getSession()->remove('userReponses'); 
                 return $this->render('results.html.twig', [
-                    'score' => $score
+                    'results' => $results,
+                    'score' => $score,
+                    'maxScore' => count($results)
                 ]);
             }
         }
-        
+
         return $this->render('quiz.html.twig', [
             'form' => $form->createView(),
         ]);
