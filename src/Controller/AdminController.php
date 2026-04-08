2a0088d03b6e569ca55c54fb347cc1dcda6503c6
melina <boukharimelina44@gmail.com>
2026-04-08 22:04:10 +0200

    rajout controlleur crud et twig crud

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,9 +1,11 @@
-<?php 
+<?php
+
 namespace App\Controller;
 
 use App\Entity\Answers;
 use App\Entity\Questions;
 use App\Form\QuestionsType;
+use App\Repository\QuestionsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,6 +14,16 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class AdminController extends AbstractController
 {
+    #[Route('/admin', name: 'admin_question_list')]
+    public function list(QuestionsRepository $repo): Response
+    {
+        $questions = $repo->findAll();
+
+        return $this->render('list.html.twig', [
+            'questions' => $questions,
+        ]);
+    }
+
     #[Route('/admin/question/new', name: 'admin_question_new')]
     public function new(Request $request, EntityManagerInterface $em): Response
     {
@@ -19,7 +31,7 @@ class AdminController extends AbstractController
 
         for ($i = 0; $i < 4; $i++) {
             $answer = new Answers();
-            $question->addAnswer($answer); 
+            $question->addAnswer($answer);
         }
 
         $form = $this->createForm(QuestionsType::class, $question);
@@ -32,8 +44,34 @@ class AdminController extends AbstractController
             return $this->redirectToRoute('home');
         }
 
-        return $this->render('addQuestion.html.twig', [
+        return $this->render('new.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/admin/question/edit/{id}', name: 'admin_question_edit')]
+    public function edit(Questions $question, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(QuestionsType::class, $question);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('admin_question_list');
+        }
+
+        return $this->render('edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/question/delete/{id}', name: 'admin_question_delete')]
+    public function delete(Questions $question, EntityManagerInterface $em): Response
+    {
+        $em->remove($question);
+        $em->flush();
+
+        return $this->redirectToRoute('admin_question_list');
     }
 }
